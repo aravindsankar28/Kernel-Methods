99d963cb8759f00b6abb686af4f37701ebc1d948
Ramnandan Krishnamurthy <nandparikrish@gmail.com>
2015-02-04 11:13:47 +0530

    Surface plot

--- Assignment_1/Code/Q2.m
+++ Assignment_1/Code/Q2.m
@@ -2,6 +2,8 @@ N_values = [20,100,1000]; % training set size
 M_values = [3,9,15,200];  % no of basis functions
 lambda_values = [0,10^(-6),10^(-5),10^(-4),10^(-3),10^(-2),10^(-1),1,2,5]; % regularization parameter
 
+N = 100;
+
 % read in training dataset 
 fId = fopen(strcat('../Data/group2/bivariateData/group2_train',num2str(N),'.txt'),'r');
 sizeTrain = [N 3];
@@ -21,8 +23,8 @@ testSet = fscanf(fId,'%f %f %f',sizeTest);
 fclose(fId);
 
 % Plots on varying M - note lambda = 0 here
-N_values = [20]; % training set size
-M_values = [3,9,15,200];  % no of basis functions
+N_values = [100]; % training set size
+M_values = [5];  % no of basis functions
 
 target_output = trainSet(:,3);
 for i = 1:length(N_values)
@@ -31,8 +33,21 @@ for i = 1:length(N_values)
         model_output = designMat*coeffs;
         
         fig1 = figure;
-        plot3(trainSet(:,1),trainSet(:,2),trainSet(:,3),'ro',trainSet(:,1),trainSet(:,2),trainSet(:,3),'bo');
+        plot3(trainSet(:,1),trainSet(:,2),trainSet(:,3),'ro');
         %, axis([-90,130,-20,150,-10,80]
-        legend('t','y(x,w)');
+        
+        [x1,x2] = meshgrid(linspace(-20,10));
+        y = zeros(size(x1,1),size(x1,2));
+        basisVec = zeros(M_values(j),1);
+        for k = 1:size(x1,1)
+            for l = 1:size(x1,2)
+                for m = 1:M_values(j)
+                    basisVec(m,1) = exp(-((x1(k,l)-centroids(m,1))^2+(x2(k,l)-centroids(m,2))^2))/widthParam(m,1);
+                end 
+                y(k,l) = basisVec'*coeffs;
+            end
+        end
+        surf(x1,x2,y);
+        %legend('t','y(x,w)');
     end
 end
