f494ae634395fe82bbcd09f9ca1786de5182366a
Aravind Sankar <aravindsankar28@gmail.com>
2015-03-11 18:26:22 +0530

    Changed to BIC

--- Assignment_2/Classification/Code/nonlinearlySeparable/bayes.m
+++ Assignment_2/Classification/Code/nonlinearlySeparable/bayes.m
@@ -4,7 +4,7 @@ hold on;
 % Here 50 is the number of times the EM (GMM is fit) is run to make sure
 % that correct result is obtained.
 
-num1 = find_no_mixture_comps(class1_train,6,50); % Best is 3
+num1 = find_no_mixture_comps(class1_train,6,100); % Best is 3
 gm_1 = fitgmdist(class1_train,num1);
 ezcontour(@(x,y)pdf(gm_1,[x y]));
 
@@ -12,7 +12,7 @@ figure;
 scatter(train(:,1),train(:,2),'*');
 hold on;
 
-num2 = find_no_mixture_comps(class2_train,10,50);
+num2 = find_no_mixture_comps(class2_train,10,100);
 gm_2 = fitgmdist(class2_train,num2);
 ezcontour(@(x,y)pdf(gm_2,[x y]));
 % Still can't figure out optimal no. of components for class 2 .. the AIC
--- Assignment_2/Classification/Code/nonlinearlySeparable/find_no_mixture_comps.m
+++ Assignment_2/Classification/Code/nonlinearlySeparable/find_no_mixture_comps.m
@@ -2,14 +2,14 @@ function num_comps = find_no_mixture_comps(data,MaxComp,iters)
 
 num_comp_best = zeros(MaxComp,1);
 for iter = 1:iters
-    AIC = zeros(MaxComp,1);
+    BIC = zeros(MaxComp,1);
     GMModels = cell(MaxComp,1);
     options = statset('MaxIter',100);
     for k = 1:MaxComp
         GMModels{k} = fitgmdist(data,k,'Options',options);
-        AIC(k)= GMModels{k}.AIC;
+        BIC(k)= GMModels{k}.BIC;
     end
-    [minAIC,numComponents] = min(AIC);
+    [minBIC,numComponents] = min(BIC);
     num_comp_best(numComponents) = num_comp_best(numComponents) + 1;
 end
 
