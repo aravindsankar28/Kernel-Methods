2ddcbe50b957b0482e286ed0a778171b8f8a8844
Aravind Sankar <aravindsankar28@gmail.com>
2015-03-14 17:44:57 +0530

    Latest update

--- Assignment_2/Regression/Code/univariate/MLFFNN.m
+++ Assignment_2/Regression/Code/univariate/MLFFNN.m
@@ -3,27 +3,43 @@ load_data;
 inputs = data_input';
 targets = data_target';
 
-% Create a Pattern Recognition net
-hiddenLayerSize = 10;
-net = fitnet(hiddenLayerSize);
-net.performFcn = 'mse';
-net.plotFcns = {'plotperform','plottrainstate','ploterrhist', ...
-  'plotregression', 'plotfit'};
+val_performances = zeros(20,1);
 
-net.divideFcn = 'divideblock';  % Divide blockwise
-net.divideParam.trainRatio = (2/3);
-net.divideParam.valRatio = (1/6);
-net.divideParam.testRatio = (1/6);
+for i = 1:40
+
+    hiddenLayerSize = i*5;
+    net = fitnet(hiddenLayerSize);
+    net.performFcn = 'mse';
+    net.trainFcn = 'trainlm';
+    net.plotFcns = {'plotperform','plottrainstate','ploterrhist', ...
+      'plotregression', 'plotfit'};
+
+    net.divideFcn = 'divideblock';  % Divide blockwise
+    net.divideParam.trainRatio = (2/3);
+    net.divideParam.valRatio = (1/6);
+    net.divideParam.testRatio = (1/6);
 
 
-% Train the net
-[net,tr] = train(net,inputs,targets);
+    % Train the net
+    [net,tr] = train(net,inputs,targets);
 
-% Test the net
-outputs = net(inputs);
-errors = gsubtract(outputs,targets);
-performance = perform(net,targets,outputs);
+    % Test the net
+    outputs = net(inputs);
+    errors = gsubtract(outputs,targets);
+    performance = perform(net,targets,outputs);
+    
+    valTargets = targets  .* tr.valMask{1};
+    valPerformance = perform(net,valTargets,outputs);
 
+    val_performances(i)= valPerformance; 
 % View the net
-view(net)
+%view(net);
+
+
+end
+
+[~,idx] = min(val_performances);
+
+idx*5
+
 
--- Assignment_2/Regression/Code/univariate/load_data.m
+++ Assignment_2/Regression/Code/univariate/load_data.m
@@ -1,9 +1,11 @@
 sigma = 0.1;
-
-val_data = samplePoints(sigma,250);
-test_data = samplePoints(sigma,250);
-train_data = samplePoints(sigma,1000);
-
+% 
+% val_data = samplePoints(sigma,250);
+% test_data = samplePoints(sigma,250);
+% train_data = samplePoints(sigma,1000);
+% 
+% save('data.mat','train_data','val_data','test_data');
+load('data.mat');
 data_input = [train_data(:,1);val_data(:,1);test_data(:,1)];
 data_target = [train_data(:,2);val_data(:,2);test_data(:,2)];
 
