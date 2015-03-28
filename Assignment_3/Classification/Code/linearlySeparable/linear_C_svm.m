a9e2559574ee170845de8632c524ed1f4620964c
Ramnandan Krishnamurthy <nandparikrish@gmail.com>
2015-03-28 14:33:41 +0530

    C-SVM linear

--- Assignment_3/Classification/Code/linearlySeparable/linear_C_svm.m
+++ Assignment_3/Classification/Code/linearlySeparable/linear_C_svm.m
@@ -1,4 +1,13 @@
 % Load and scale data
 load_data
 
+% Cross-validation to identify best C
+best_C = 1.0;
 
+% Final model train
+cmd = ['-s 0 -t 0 -c ',num2str(best_C)];
+model = ovrtrain(target_train,train,cmd);
+
+% Test
+[pred ac decv] = ovrpredict(target_test, test, model);
+fprintf('Accuracy = %g%%\n', ac * 100);
