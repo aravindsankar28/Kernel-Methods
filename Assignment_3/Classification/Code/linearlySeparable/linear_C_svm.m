e67081f75673f7230df36954f0257b22b3169293
Ramnandan Krishnamurthy <nandparikrish@gmail.com>
2015-03-28 19:10:49 +0530

    Finished linear SVM

--- Assignment_3/Classification/Code/linearlySeparable/linear_C_svm.m
+++ Assignment_3/Classification/Code/linearlySeparable/linear_C_svm.m
@@ -2,12 +2,68 @@
 load_data
 
 % Cross-validation to identify best C
-best_C = 1.0;
+bestcv = 0;
+for log2c = -1:2:3,
+   cmd = ['-q -s 0 -t 0 -c ',num2str(2^log2c)];
+   model = ovrtrain(target_train,train,cmd);
+   [pred ac decv] = ovrpredict(target_val, val, model);
+   if (ac >= bestcv),
+     bestcv = ac; best_C = 2^log2c; 
+   end
+   fprintf('log2c=%g acc=%g (best C=%g, acc=%g)\n', log2c, ac, best_C, bestcv);
+end
 
 % Final model train
-cmd = ['-s 0 -t 0 -c ',num2str(best_C)];
+cmd = ['-q -s 0 -t 0 -c ',num2str(best_C)];
 model = ovrtrain(target_train,train,cmd);
 
-% Test
-[pred ac decv] = ovrpredict(target_test, test, model);
-fprintf('Accuracy = %g%%\n', ac * 100);
+% Testing
+[pred_train ac decv] = ovrpredict(target_train, train, model);
+fprintf('Train Accuracy = %g%%\n', ac * 100);
+[pred_val ac decv] = ovrpredict(target_val, val, model);
+fprintf('Validation Accuracy = %g%%\n', ac * 100);
+[pred_test ac decv] = ovrpredict(target_test, test, model);
+fprintf('Test Accuracy = %g%%\n', ac * 100);
+
+% Confusion matrices
+fprintf('Train confusion matrix')
+[C_train,order1] = confusionmat(target_train,pred_train);
+C_train
+
+fprintf('Validation confusion matrix')
+[C_val,order2] = confusionmat(target_val,pred_val);
+C_val
+
+fprintf('Test confusion matrix')
+[C_test,order3] = confusionmat(target_test,pred_test);
+C_test
+
+% Decision region plot for training data
+xrange = [-16 20];
+yrange = [-16 20];
+inc = 0.1;
+[x, y] = meshgrid(xrange(1):inc:xrange(2), yrange(1):inc:yrange(2)); 
+image_size = size(x); 
+xy = [x(:) y(:)]; % make (x,y) pairs as a bunch of row vectors.
+
+temp_labels = ones(size(xy,1),1);
+[pred ac decv] = ovrpredict(temp_labels, xy, model);
+
+figure;
+decisionmap = reshape(pred, image_size);
+imagesc(xrange,yrange,decisionmap);
+hold on;
+set(gca,'ydir','normal');
+cmap = [1 0.8 0.8; 0.95 1 0.95; 0.9 0.9 1; 0.6 0.7 1];
+colormap(cmap);
+
+plot(class1_train(:,1),class1_train(:,2),'r*');
+plot(class2_train(:,1),class2_train(:,2),'g*');
+plot(class3_train(:,1),class3_train(:,2),'b*');
+plot(class4_train(:,1),class4_train(:,2),'k*');
+
+a = xlabel('$x_1$');
+b = ylabel('$x_2$');
+set(a,'Interpreter','latex');
+set(b,'Interpreter','latex');
+title('Decision region plot');
