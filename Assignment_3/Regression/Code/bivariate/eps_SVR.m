cd23817de7fc2a24129daedfdcebb22dbf798d13
Aravind Sankar <aravindsankar28@gmail.com>
2015-04-02 12:44:03 +0530

    Epsilon SVR bivariate done
    Need to check the best params though. Takes time to run.

--- Assignment_3/Regression/Code/bivariate/eps_SVR.m
+++ Assignment_3/Regression/Code/bivariate/eps_SVR.m
@@ -8,20 +8,20 @@ best_C = 0;
 best_g = 0;
 best_p = 0;
 
-mse_train_eps = zeros(7,1);
-mse_val_eps = zeros(7,1);
-mse_test_eps = zeros(7,1);
+mse_train_eps = zeros(8,1);
+mse_val_eps = zeros(8,1);
+mse_test_eps = zeros(8,1);
 
 
 i = 1;
-for log2p = -3:1:3
+for log2p = -4:1:3
     log2p
     local_best_cv = Inf;
     local_best_C = 0;
     local_best_g = 0;
     
-    for log2c = -3:1:6
-        for log2g = -3:1:6
+    for log2c = -2:1:9
+        for log2g = -10:1:3
             cmd = ['-q -s 3 -t 2 -c ',num2str(2^log2c),' -g ',num2str(2^log2g),' -p ',num2str(2^log2p)];
             model = svmtrain(target_train,train,cmd);
             
@@ -53,14 +53,14 @@ bestcv,best_C,best_g,best_p
 
 
 % MSE plot w.r.t eps
-eps_range = 2.^(-3:1:3);
+eps_range = 2.^(-4:1:3);
 plot(eps_range,mse_train_eps,eps_range,mse_val_eps,eps_range,mse_test_eps);
 legend('Train','Validation','Test');
 
 a = title('MSE on varying $\epsilon$');
 set(a,'Interpreter','latex');
 
-return;
+
 % Final model train
 cmd = ['-s 3 -t 2 -c ',num2str(best_C),' -g ',num2str(best_g), ' -p ',num2str(best_p)];
 model = svmtrain(target_train,train,cmd);
@@ -75,3 +75,99 @@ fprintf('Validation MSE = %g%\n', mse_val);
 [pred_test, ac, decv] = svmpredict(target_test, test, model);
 mse_test = ac(2);
 fprintf('Test MSE = %g%\n', mse_test);
+
+xrange = [-10 10];
+yrange = [-10 10];
+inc = 0.1;
+[x, y] = meshgrid(xrange(1):inc:xrange(2), yrange(1):inc:yrange(2)); 
+image_size = size(x); 
+xy = [x(:) y(:)]; % make (x,y) pairs as a bunch of row vectors.
+
+[pred_values, ac, decv] = svmpredict(zeros(length(xy),1), xy, model);
+
+% Plot of approximated function
+figure,plot3(xy(:,1),xy(:,2),pred_values,'r.');
+a = title('Plot of approximated function over the $x_1$-$x_2$ space');
+b = xlabel('$x_1$');
+c = ylabel('$x_2$');
+d = zlabel('f(x)');
+set(a,'Interpreter','latex');
+set(b,'Interpreter','latex');
+set(c,'Interpreter','latex');
+set(d,'Interpreter','latex');
+
+% Plots of model and target output for train data.
+
+figure,plot3(train(:,1),train(:,2),target_train,'b.',train(:,1),train(:,2),pred_train,'r.');
+
+a = title('Model output and target output for training data');
+b = xlabel('$x_1$');
+c = ylabel('$x_2$');
+
+legend('Target output','Model output');
+set(b,'Interpreter','latex');
+set(c,'Interpreter','latex');
+
+
+
+% Plots of model and target output for validation data.
+
+figure,plot3(val(:,1),val(:,2),target_val,'b.',val(:,1),val(:,2),pred_val,'r.');
+
+a = title('Model output and target output for validation data');
+b = xlabel('$x_1$');
+c = ylabel('$x_2$');
+
+legend('Target output','Model output');
+set(b,'Interpreter','latex');
+set(c,'Interpreter','latex');
+
+
+
+% Plots of model and target output for test data.
+
+figure,plot3(test(:,1),test(:,2),target_test,'b.',test(:,1),test(:,2),pred_test,'r.');
+
+a = title('Model output and target output for test data');
+b = xlabel('$x_1$');
+c = ylabel('$x_2$');
+
+legend('Target output','Model output');
+set(b,'Interpreter','latex');
+set(c,'Interpreter','latex');
+
+
+
+% Scatter plots here. train - target on x and model on y.
+x_range = -30:1:200;
+figure;
+scatter(target_train,pred_train,'k.');
+hold on;
+plot(x_range,x_range,'m');
+
+title('Scatter plot of Model Vs Target output - train data');
+xlabel('Target output');
+ylabel('Model output');
+legend('Scatter plot','Ideal y = x');
+
+% Scatter plots here. val - target on x and model on y.
+figure;
+scatter(target_val,pred_val,'r.');
+hold on;
+plot(x_range,x_range,'m');
+
+title('Scatter plot of Model Vs Target output - validation data');
+xlabel('Target output');
+ylabel('Model output');
+legend('Scatter plot','Ideal y = x');
+
+% Scatter plots here. test  - target on x and model on y.
+figure;
+scatter(target_test,pred_test,'b.');
+hold on;
+plot(x_range,x_range,'m');
+
+title('Scatter plot of Model Vs Target output - test data');
+xlabel('Target output');
+ylabel('Model output');
+legend('Scatter plot','Ideal y = x');
--- Assignment_3/Regression/Code/bivariate/load_data.m
+++ Assignment_3/Regression/Code/bivariate/load_data.m
@@ -15,24 +15,24 @@ target_train = train_data(:,3);
 target_val = val_data(:,3);
 target_test = test_data(:,3);
 
-train_scaled = train;
-val_scaled = val;
-test_scaled = test;
-% Scale data
-
-min_coord = zeros(size(train_scaled,2),1);
-max_coord = zeros(size(train_scaled,2),1);
-
-for i=1:size(train_scaled,2)
-    min_val = min(train_scaled(:,i));
-    max_val = max(train_scaled(:,i));
-    
-    min_coord(i) = min_val;
-    max_coord(i) = max_val;
-    
-    train_scaled(:,i) = (train_scaled(:,i)-min_val)/(max_val-min_val);
-    val_scaled(:,i) = (val_scaled(:,i)-min_val)/(max_val-min_val);
-    test_scaled(:,i) = (test_scaled(:,i)-min_val)/(max_val-min_val);
-end
+% train_scaled = train;
+% val_scaled = val;
+% test_scaled = test;
+% % Scale data
+% 
+% min_coord = zeros(size(train_scaled,2),1);
+% max_coord = zeros(size(train_scaled,2),1);
+% 
+% for i=1:size(train_scaled,2)
+%     min_val = min(train_scaled(:,i));
+%     max_val = max(train_scaled(:,i));
+%     
+%     min_coord(i) = min_val;
+%     max_coord(i) = max_val;
+%     
+%     train_scaled(:,i) = (train_scaled(:,i)-min_val)/(max_val-min_val);
+%     val_scaled(:,i) = (val_scaled(:,i)-min_val)/(max_val-min_val);
+%     test_scaled(:,i) = (test_scaled(:,i)-min_val)/(max_val-min_val);
+% end
 
 
